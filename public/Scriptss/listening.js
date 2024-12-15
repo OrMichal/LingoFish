4956ec64794864e6150a42264f4f7eb7c80d1c43
OrMichal <misaorsah5@gmail.com>
2024-12-15 16:25:23 +0100

    načata js práce na poslechu

--- public/Scriptss/listening.js
+++ public/Scriptss/listening.js
@@ -1,6 +1,8 @@
-const exerButt = document.getElementById("navChoice-item");
+const exerButt = document.getElementById("exerButt");
 const exercise = document.getElementById("exerciseDiv");
+const heading = document.getElementById("exerHead");
 
 exerButt.addEventListener('click', () => {
-
+    heading.innerHTML = exerButt.value;
 });
+
