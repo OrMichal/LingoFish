fe4efec3e44188fdb86c03aa06ffd0eff3d83196
OrMichal <misaorsah5@gmail.com>
2024-12-16 22:38:38 +0100

    ...

--- public/Scriptss/listening.js
+++ public/Scriptss/listening.js
@@ -1,8 +1,17 @@
-const exerButt = document.getElementById("exerButt");
+const exerButts = document.querySelectorAll("#exerButt");
 const exercise = document.getElementById("exerciseDiv");
 const heading = document.getElementById("exerHead");
+const audioPlayer = document.getElementById("audio");
 
-exerButt.addEventListener('click', () => {
-    heading.innerHTML = exerButt;
+exerButts.forEach((button) => {
+    button.addEventListener('click', () => {
+        const exName = button.querySelector(".nChItem").textContent;
+
+        heading.innerHTML = exName;
+        exercise.querySelector(".audio").querySelector(".source").src = "../audio/" + exName + ".mp3";
+        audioPlayer.style.display = "flex";
+        document.getElementById("questons").style.display = "block";
+        exercise.querySelector(".audio").load();
+    });
 });
 
